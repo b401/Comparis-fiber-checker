a39d03887f2564a72318981b83f7059309d712a0
sec <sec@uauth.io>
2021-01-15 15:53:16 +0100

    added swisscom & beautified code

--- background.js
+++ background.js
@@ -4,24 +4,51 @@
 // Return:
 //  Promise
 function is_it_salty_in_here(home) {
- // create request to create a new session
-  fetch("https://fiber.salt.ch/",{
-	method: 'GET'  
-  });
+    // create request to create a new session
+    fetch("https://fiber.salt.ch/en", {
+        method: 'GET'
+    });
 
-  return fetch("https://fiber.salt.ch/fiber-ui-service/public/eligibility/fiber/address/check", {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(home)
-  })
-  .then(response => response.json())
-  .catch(error => {
-    console.log(error);
-  });
+    return fetch("https://fiber.salt.ch/fiber-ui-service/public/eligibility/fiber/address/check", {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(home)
+        })
+        .then(response => response.json())
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+// Swisscom Request
+function is_it_swisscom_in_here(home) {
+    let new_body = ({
+        language: "en",
+        address: {
+            zipCode4: home.zip_code,
+            city: home.city,
+            street: home.street,
+            houseNumber: home.house_number
+        }
+    });
+
+    return fetch("https://www.swisscom.ch/map-api/onlinenslg/lineinfo", {
+            method: "POST",
+            credentials: 'include',
+            body: JSON.stringify(new_body),
+            "headers": {
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(response => response.json())
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 // create a async wrapper function for mutliple eligibility checks
@@ -30,13 +57,15 @@ function is_it_salty_in_here(home) {
 // Return:
 //  callback: object(id,salt,init7)
 async function wrapper(home, fn) {
-  let salt = await is_it_salty_in_here(home.address);
-  let init7 = await is_it_init7_in_here(home.address);
-  fn({
-    id: home.id_find,
-    salt: salt,
-    init7: init7
-  });
+    let salt = await is_it_salty_in_here(home.address);
+    let init7 = await is_it_init7_in_here(home.address);
+    let swisscom = await is_it_swisscom_in_here(home.address);
+    fn({
+        id: home.id_find,
+        salt: salt,
+        init7: init7,
+        swisscom: swisscom
+    });
 }
 
 // Fetch infromation from init7 api with a GET request
@@ -46,20 +75,19 @@ async function wrapper(home, fn) {
 //  Promise
 function is_it_init7_in_here(home) {
     return fetch("https://api.init7.net/check/address/" +
-      home.zip_code+"/" +
-      home.street+"/" +
-      home.house_number
-    ,{
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json'
-    },
-  })
-  .then(response => response.json())
-  .catch(error => {
-    console.log(error);
-  });
+            home.zip_code + "/" +
+            home.street + "/" +
+            home.house_number, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+            })
+        .then(response => response.json())
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 // Use chrome API to listen for new requests.
@@ -71,11 +99,10 @@ function is_it_init7_in_here(home) {
 //  sendResponse: cb
 // Return:
 //  Promise
-chrome.extension.onRequest.addListener(function (message, sender, sendResponse) {
- if (message.title === 'showResponse'){
-    wrapper(message.homes, sendResponse, function(values){
-      sendResponse(values);
-    });
- };
+chrome.extension.onRequest.addListener(function(message, sender, sendResponse) {
+    if (message.title === 'showResponse') {
+        wrapper(message.homes, sendResponse, function(values) {
+            sendResponse(values);
+        });
+    };
 });
-
--- contentscript.js
+++ contentscript.js
@@ -1,65 +1,81 @@
 function main(newobject) {
-	let id = newobject.parentElement.parentElement;
-	let adress = id.innerText;
-	let zip_code = adress.match(/[\d]{4}\s/i)[0].trim();
-	let city = adress.match(/[\d]{4}\s.+/i)[0].replace(zip_code,"").trim();
-	let street_number = adress.replace(zip_code,"").replace(city,"").replace(",","").trim();
+    let id = newobject.parentElement.parentElement;
+    let adress = id.innerText;
+    let zip_code = adress.match(/[\d]{4}\s/i)[0].trim();
+    let city = adress.match(/[\d]{4}\s.+/i)[0].replace(zip_code, "").trim();
+    let street_number = adress.replace(zip_code, "").replace(city, "").replace(",", "").trim();
 
-	if (street_number !== '') {
-      let house_number = "0";
-      if(street_number !== null && /[0-9]/.test(street_number)) {
-        try {
-			house_number = street_number.match(/(?=\w)\w[0-9A-za-z]{0,}$/)[0].trim();
-	}catch{
-		house_number = "0";
-	}
-      }
-      let street = street_number.replace(house_number,"").trim();
-
-      let iseligible = ({
-	 id,
-        id_find: btoa(street.replaceAll(" ","_")+house_number.replaceAll(" ","_")),
-        address: {
-          city: city,
-          house_number: house_number,
-          language: "EN",
-          new_order: true,
-          street: street,
-          zip_code: zip_code
+    if (street_number !== '') {
+        let house_number = "0";
+        if (street_number !== null && /[0-9]/.test(street_number)) {
+            try {
+                house_number = street_number.match(/(?=\w)\w[0-9A-za-z]{0,}$/)[0].trim();
+            } catch {
+                house_number = "0";
+            }
         }
-      });
+        let street = street_number.replace(house_number, "").trim();
 
-      iseligible.id.insertAdjacentHTML('beforeend'," <br /> <img id='"+iseligible.id_find+"' width='20px' style='margin-left:5px' src='"+ chrome.extension.getURL("images/loading.png") +"'></img>");
+        let iseligible = ({
+            id,
+            id_find: btoa(street.replaceAll(" ", "_") + house_number.replaceAll(" ", "_")),
+            address: {
+                city: city,
+                house_number: house_number,
+                language: "EN",
+                new_order: true,
+                street: street,
+                zip_code: zip_code
+            }
+        });
 
-      chrome.extension.sendRequest({title: 'showResponse', homes: iseligible},
-      function(response){
-        if(response.init7.fiber7 === true){
-	   document.getElementById(response.id).insertAdjacentHTML("afterend"," <img width='20px' style='margin-left:5px' src='"+ chrome.extension.getURL("images/Init7_logo.png") +"'></img>");
-        }
-        if(response.salt.valid === true){
-	   document.getElementById(response.id).insertAdjacentHTML("afterend","<img width='20px' style='margin-left:5px' src='"+ chrome.extension.getURL("images/salt_logo.png") +"'></img>");
-        }
+        iseligible.id.insertAdjacentHTML('beforeend', " <br /> <img id='" + iseligible.id_find + "' width='20px' style='margin-left:5px' src='" + chrome.extension.getURL("images/loading.png") + "'></img>");
+        iseligible.id.insertAdjacentHTML('beforeend', "<div id='" + iseligible.id_find + "_img'></div>");
+        iseligible.id.insertAdjacentHTML('beforeend', "<div id='" + iseligible.id_find + "_speed'></div>");
 
-        if(response.init7.vdsl) {
-			document.getElementById(response.id).insertAdjacentHTML("afterend","<p style='font-size:0.6em'>Init7 VDSL:"+ response.init7.vdsl_down +"/"+ response.init7.vdsl_up +"</p>");
-          }
+        chrome.extension.sendRequest({
+                title: 'showResponse',
+                homes: iseligible
+            },
+            function(response) {
+                if (response.init7 !== undefined && response.init7.fiber7 === true) {
+                    document.getElementById(response.id + "_img").innerHTML += "<img width='20px' style='margin-left:5px' src='" + chrome.extension.getURL("images/Init7_logo.png") + "'></img>";
+                    //document.getElementById(response.id).insertAdjacentHTML("afterend"," <img width='20px' style='margin-left:5px' src='"+ chrome.extension.getURL("images/Init7_logo.png") +"'></img>");
+                }
+                if (response.salt !== undefined && response.salt.valid === true) {
+                    document.getElementById(response.id + "_img").innerHTML += "<img width='20px' style='margin-left:5px' src='" + chrome.extension.getURL("images/salt_logo.png") + "'></img>";
+                }
 
-	document.getElementById(response.id).remove(); 
-     });
-   }
-  }
+                if (response.init7.vdsl === true) {
+                    document.getElementById(response.id + "_speed").innerHTML += "<p style='font-size:0.8em'>Init7 VDSL:" + response.init7.vdsl_down / 100 + " Mbit/s / " + response.init7.vdsl_up / 100 + " Mbit/s</p>";
+                }
+
+                if (response.swisscom.broadbandInfo !== null && response.swisscom.broadbandInfo.available === true) {
+                    document.getElementById(response.id + "_img").innerHTML += "<img width='20px' style='margin-left:5px' src='" + chrome.extension.getURL("images/swisscom_logo.png") + "'></img>";
+                    document.getElementById(response.id + "_speed").innerHTML += "<p style='font-size:0.8em'>Swisscom: max. " + response.swisscom.broadbandInfo.maxDownSpeed + " / " + response.swisscom.broadbandInfo.maxUpSpeed + "</p>";
+                    if (response.swisscom.broadbandInfo.extensionInfo !== null) {
+                        document.getElementById(response.id + "_speed").innerHTML += "<p style='font-size:0.6em'>Swisscom Info:" + response.swisscom.extensionInfo + "</p>";
+                    }
+                }
+                document.getElementById(response.id).remove();
+            });
+    }
+}
 
 var observer = new WebKitMutationObserver(function(mutations) {
-	mutations.forEach(function(mutation) {
-		if(mutation.addedNodes.length > 0) {
-			if(mutation.addedNodes[0].tagName === 'DIV'){
-				var xpath = document.evaluate(".//*[local-name() = 'svg' and @data-icon='map-marker-alt']",mutation.addedNodes[0],null,XPathResult.FIRST_ORDERED_NODE_TYPE,null);
-				if(xpath.singleNodeValue !== null && xpath.singleNodeValue !== undefined){
-					main(xpath.singleNodeValue);
-				}
-			}
-		}
-	});
+    mutations.forEach(function(mutation) {
+        if (mutation.addedNodes.length > 0) {
+            if (mutation.addedNodes[0].tagName === 'DIV') {
+                var xpath = document.evaluate(".//*[local-name() = 'svg' and @data-icon='map-marker-alt']", mutation.addedNodes[0], null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+                if (xpath.singleNodeValue !== null && xpath.singleNodeValue !== undefined) {
+                    main(xpath.singleNodeValue);
+                }
+            }
+        }
+    });
 });
 
-observer.observe(document, {childList: true, subtree: true});
+observer.observe(document, {
+    childList: true,
+    subtree: true
+});
